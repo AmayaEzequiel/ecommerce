c3b5cdf59296228bc2766cfbda1fc8352e22f4a8
Ezequiel Amaya <116738528+AmayaEzequiel@users.noreply.github.com>
2026-05-14 04:27:41 +0000

    cam,bios

---
+++ models/Product.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Product = sequelize.define('Product', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING(100),
+    allowNull: false
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: true
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2),
+    allowNull: false
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    defaultValue: 0
+  },
+  image: {
+    type: DataTypes.STRING(255),
+    allowNull: true
+  }
+}, {
+  tableName: 'Products',
+  timestamps: true
+});
+
+module.exports = Product;
